226da8aee65493d1fc0cf57e2cc9e0afb5e47338
anthony GODDERIDGE <godderidgeanthony@gmail.com>
2021-03-22 13:15:12 +0100

    projet A jour 2.0

--- ProjetFilRouge/src/test/java/com/aston/filrouge/ProjetFilRougeApplicationTests.java
+++ ProjetFilRouge/src/test/java/com/aston/filrouge/ProjetFilRougeApplicationTests.java
@@ -1,26 +1,13 @@
 package com.aston.filrouge;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import com.aston.filrouge.model.Produit;
 
 @SpringBootTest
 class ProjetFilRougeApplicationTests {
 
 	@Test
 	void contextLoads() {
-		
 	}
 
-	@Test
-	void test1() {
-		Produit p = new Produit();
-		System.err.println(p.getDescription());
-		assertTrue(p.getDescription().isEmpty());
-		
-	}
 }

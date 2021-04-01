a728e4fa3a8f49001178180cafd61057116ade98
Anthony GODDERIDGE <godderidgeanthony@gmail.com>
2021-04-01 12:35:57 +0200

    elk a test

--- ProjetFilRouge-Back/src/main/java/com/aston/filrouge/controller/EmployeeController.java
+++ ProjetFilRouge-Back/src/main/java/com/aston/filrouge/controller/EmployeeController.java
@@ -23,8 +23,8 @@ import com.aston.filrouge.dto.EmployeeDto;
 import com.aston.filrouge.model.Employee;
 import com.aston.filrouge.repository.EmployeeRepository;
 
-@CrossOrigin(origins = "http://192.168.0.37:4200")
-//@CrossOrigin(origins = "http://164.132.212.109:4200")
+//@CrossOrigin(origins = "http://192.168.0.37:4200")
+@CrossOrigin(origins = "http://164.132.212.109:4200")
 //@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api")

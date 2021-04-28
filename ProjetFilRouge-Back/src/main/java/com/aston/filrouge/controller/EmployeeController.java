0bf6e2a8910dd534be5e409ef1ac9cd11b56afe3
Anthony GODDERIDGE <godderidgeanthony@gmail.com>
2021-04-28 09:35:38 +0200

    filrouge

--- ProjetFilRouge-Back/src/main/java/com/aston/filrouge/controller/EmployeeController.java
+++ ProjetFilRouge-Back/src/main/java/com/aston/filrouge/controller/EmployeeController.java
@@ -23,7 +23,7 @@ import com.aston.filrouge.dto.EmployeeDto;
 import com.aston.filrouge.model.Employee;
 import com.aston.filrouge.repository.EmployeeRepository;
 
-@CrossOrigin(origins = "http://170.75.170.145:4200")
+@CrossOrigin(origins = "http://170.75.170.247:4200")
 //@CrossOrigin(origins = "http://fil-rouge-1.xyz:4200")
 //@CrossOrigin(origins = "http://localhost:4200")
 @RestController
